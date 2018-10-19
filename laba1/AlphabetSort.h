96f357b3e8ba0c60621fa662cf9429c2cb1f92d1
Filippov Konstantin <lolkazavr1@gmail.com>
2018-10-19 13:54:51 +0300

    Удалены ненужные файлы

--- laba1/AlphabetSort.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "stdafx.h"
-#include "Price.h"
-
-void AlphabetSort(Price *P,int size);
--- laba1/FindShop.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "stdafx.h"
-#include "Price.h"
-
-
-void FindShop(Price *P,int size);
